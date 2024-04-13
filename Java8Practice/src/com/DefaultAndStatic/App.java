f478b2a054b3a61968a9ca1019bce6bc4cb714b4
nilima1991 <nilimamankar@gmail.com>
2024-04-13 16:21:58 +0530

    defaultandStatic

--- Java8Practice/src/com/DefaultAndStatic/App.java
+++ Java8Practice/src/com/DefaultAndStatic/App.java
@@ -1,5 +1,11 @@
 package com.DefaultAndStatic;
 
-public class App {
-
+public class App implements I1 {
+	public static void main(String[] args) {
+		I1 i=new App();
+		i.m1();
+		I1.m2();
+	}
+	
 }
+
